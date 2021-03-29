b09308857a938378b60ca5e7eee9e70f92e5084d
johan@dospuntos.no <johan@dospuntos.no>
2021-03-29 14:49:38 -0400

    Removed need for axios

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -2,29 +2,22 @@ import { useState, useEffect } from "react";
 import Filters from "./Filters";
 import Projects from "./Projects";
 import { useTranslation } from "react-i18next";
-import axios from "axios";
 
 export default function Portfolio({ lang }) {
   const [projects, setProjects] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [tagFilter, setTagFilter] = useState("");
   const { i18n } = useTranslation();
-  /* 
-  const [filterCategory, setFilterCategory] = useState("");
-  const [found, setFound] = useState(false); */
 
   useEffect(() => {
     getAllProjects(i18n.language);
   }, [i18n.language]);
 
   function getAllProjects(lang = "en") {
-    //setIsLoading(true);
-    axios
-      .get("./data." + lang + ".json")
-      // Add some delay for testing
-      .then((x) => new Promise((resolve) => setTimeout(() => resolve(x), 1000)))
-      .then((response) => {
-        setProjects(response.data);
+    fetch(`./data.${lang}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProjects(data);
         setIsLoading(false);
       });
   }
