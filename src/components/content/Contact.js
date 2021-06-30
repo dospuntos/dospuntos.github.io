09b425dbaef6dad14679a9b7d7eea47ce7af777c
johan@dospuntos.no <johan@dospuntos.no>
2021-06-30 02:15:57 -0400

    Misc layout changes

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -6,15 +6,32 @@ function Contact() {
   const { t } = useTranslation();
   return (
     <div className="content-wrap">
+      <div className="contact-intro">
+        <h3>{t("CONTACT_INTRO")}</h3>
+      </div>
       <div className="pure-g">
         <div className="pure-u-1 pure-u-md-3-5">
           <Form />
         </div>
         <div className="pure-u-1 pure-u-md-2-5">
-          <ul className="contact-information">
-            <li>{t("CONTACT_ADDRESS")}</li>
-            <li>{t("CONTACT_PHONE")}</li>
-            <li>{t("CONTACT_EMAIL")}</li>
+          <h3 className="text-center">{t("CONTACT_INFORMATION_HEADER")}</h3>
+          <p className="text-center">{t("CONTACT_INFORMATION_TEXT")}</p>
+          <img src="/img/contact-us.jpg" alt="Contact Information" />
+          <ul className="contact-information text-center">
+            <li style={{ whiteSpace: "pre" }}>{t("CONTACT_ADDRESS")}</li>
+            <li>
+              <a className="pure-button" href="tel:+59165605287">
+                {t("CONTACT_PHONE")}
+              </a>
+            </li>
+            <li>
+              <a
+                className="pure-button"
+                href="m&#97;ilto&#58;inf&#111;&#64;d%6&#70;s%70&#117;%&#54;Et&#37;6F&#115;&#46;&#110;&#111;"
+              >
+                {t("CONTACT_EMAIL")}
+              </a>
+            </li>
           </ul>
         </div>
       </div>
