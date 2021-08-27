297a76c553fba77ee6017c0f92dfa98d2ca56349
johan@dospuntos.no <johan@dospuntos.no>
2021-08-27 14:14:32 -0400

    Google fonts display swap

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -12,18 +12,24 @@ export default function LanguageSelector() {
       <img
         src="img/no.gif"
         alt="Norsk"
+        width={30}
+        height={20}
         className={i18n.language === "no" ? "active" : ""}
         onClick={() => changeLanguage("no")}
       />
       <img
         src="img/en.gif"
         alt="English"
+        width={30}
+        height={20}
         className={i18n.language === "en" ? "active" : ""}
         onClick={() => changeLanguage("en")}
       />
       <img
         src="img/es.gif"
         alt="Español"
+        width={30}
+        height={20}
         className={i18n.language === "es" ? "active" : ""}
         onClick={() => changeLanguage("es")}
       />
